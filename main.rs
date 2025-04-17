df3688e6abf68634cb53f3dd41d4bb335cf4076a
Max Pintchouk <maxpintchouk4321@gmail.com>
2025-04-16 20:22:04 -0400

    Defining scope

--- main.rs
+++ main.rs
@@ -1,7 +1,8 @@
 fn main() {
-    let x: i32 = 5;
-    let _y: i32;
-
-    assert_eq!(x, 5);
-    println!("Success!!")
+    let x: i32 = 10;
+    let y: i32 = 5;
+    {
+        println!("The value of x is {} and value of y is {}", x, y);
+    }
+    println!("The value of x is {} and value of y is {}", x, y)
 }
