5d1eac7ca2c74346eb9e5926e97a815bd879e302
Max Pintchouk <maxpintchouk4321@gmail.com>
2025-04-16 20:27:57 -0400

    Shadowing to a different type

--- main.rs
+++ main.rs
@@ -1,10 +1,10 @@
 fn main() {
-    let x: i32 = 5;
-    {
-        let x = 12;
-        assert_eq!(x, 12);
-    }
-    assert_eq!(x, 5);
-    let x = 42;
-    println!("{}", x)
+    let mut x: i32 = 1;
+    x = 7;
+    x += 3;
+
+    let y = 4;
+    let y = "I can also be bound to text";
+
+    println!("Success!")
 }
