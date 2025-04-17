9a10d588ad83379c737c543e3caf4f51589f406f
Max Pintchouk <maxpintchouk4321@gmail.com>
2025-04-16 20:23:28 -0400

    Calling functions

--- main.rs
+++ main.rs
@@ -1,8 +1,8 @@
 fn main() {
-    let x: i32 = 10;
-    let y: i32 = 5;
-    {
-        println!("The value of x is {} and value of y is {}", x, y);
-    }
-    println!("The value of x is {} and value of y is {}", x, y)
+    define_x();
+}
+
+fn define_x() {
+    let x: &str = "Hello";
+    println!("{}, world", x);
 }
