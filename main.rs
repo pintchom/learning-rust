1d4f66318182be651d6b95da254b39e77e24f907
Max Pintchouk <maxpintchouk4321@gmail.com>
2025-04-16 20:29:22 -0400

    allowing unused variables

--- main.rs
+++ main.rs
@@ -1,10 +1,4 @@
+#[allow(unused_variables)]
 fn main() {
-    let mut x: i32 = 1;
-    x = 7;
-    x += 3;
-
-    let y = 4;
-    let y = "I can also be bound to text";
-
-    println!("Success!")
+    let x = 1;
 }
