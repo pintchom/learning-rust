186a80bedd449bbbe930387498bdb03055bb6950
Max Pintchouk <maxpintchouk4321@gmail.com>
2025-04-16 20:34:20 -0400

    Destructuring Assignments

--- main.rs
+++ main.rs
@@ -1,9 +1,11 @@
 fn main() {
-    let (mut x, y) = (1, 2);
-    x += 2;
+    let (x, y);
 
-    assert_eq!(x, 3);
-    assert_eq!(y, 2);
+    (x, ..) = (3, ..);
+    [.., y] = [1, 2];
+    // x = 3;
+    // y = 2;
 
-    println!("Success")
+    assert_eq!([x, y], [3, 2]);
+    println!("Success");
 }
