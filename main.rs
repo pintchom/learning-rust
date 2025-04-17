a6c919803536dfcf0ec8d5838b0fd41540451b05
Max Pintchouk <maxpintchouk4321@gmail.com>
2025-04-16 20:41:27 -0400

    type inheritance

--- main.rs
+++ main.rs
@@ -1,11 +1,13 @@
+#[allow(unused_variables, unused_assignments)]
 fn main() {
-    let (x, y): (i32, i32); // THESE CANNOT BE DIFFERENT TYPES
+    let x = 5;
+    let mut y: u32 = 5;
 
-    (x, ..) = (3, ..);
-    [.., y] = [1, 2];
-    // x = 3;
-    // y = 2;
+    y = x; // THESE TYPES NEED TO MATCH TO RE-ASSIGN IMMUTABLE
 
-    assert_eq!([x, y], [3, 2]);
-    println!("Success");
+    let _z = 10;
+
+    println!("Success")
 }
+
+// default type of x is i32, however if undefined, can inherit type of assignee (y: u32 = x: undefined_type)
