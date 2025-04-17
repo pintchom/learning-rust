e47e03b5dab37e70bad218401af8b8eee9f29dfb
Max Pintchouk <maxpintchouk4321@gmail.com>
2025-04-16 20:43:49 -0400

    converting to diifferent bit arrangement

--- main.rs
+++ main.rs
@@ -1,13 +1,9 @@
 #[allow(unused_variables, unused_assignments)]
 fn main() {
-    let x = 5;
-    let mut y: u32 = 5;
+    let v: u8 = 7245_u16 as u8;
+    println!("Success, {}", v)
 
-    y = x; // THESE TYPES NEED TO MATCH TO RE-ASSIGN IMMUTABLE
-
-    let _z = 10;
-
-    println!("Success")
+    // Bit overflow example if converting to smaller bit arrangement
 }
 
 // default type of x is i32, however if undefined, can inherit type of assignee (y: u32 = x: undefined_type)
